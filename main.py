5c1ce7ebe6f877d55f5fd3717fe80c84a7c19c1e
blob404 <blob404@protonmail.ch>
2021-08-11 01:18:09 +0200

    agent with randomly updating value matrix

--- main.py
+++ main.py
@@ -1,13 +1,18 @@
 #!/usr/bin/env python3
 # The parts that aren't ready yet are commented out
-
 import wcst
 # import deep_q
-# import vanilla_q
-import torch
-import random
-import torch.nn as nn
+import vanilla_q
+# import torch
+# import torch.nn as nn
+import pandas as pd
+import os
+from pathlib import Path
+from datetime import datetime
 
+ROOT_DIR = Path(__file__).parent
+
+# TODO merge lol
 if __name__ == '__main__':
     env = wcst.WCST()
     env.reset()
@@ -18,3 +23,33 @@ if __name__ == '__main__':
         action, reward, obs, done = env.step(action)
         env.render()
         print(action, reward, obs, done)
+
+if __name__ == '__main__':
+    env = wcst.WCST()
+    env.reset()
+    agent = vanilla_q.VanillaQ(env)
+
+    df = pd.DataFrame()
+
+    for i in range(250):
+
+        s = agent._state
+        a = agent._action
+        rule = agent._env.rule
+        q = agent._q.copy(deep=True)
+
+        df = df.append({'state' : s,
+                   'prev_action' : a,
+                   'rule' : rule,
+                   'q_table' : q}, ignore_index=True)
+
+        # update state of agent and environment
+        agent.update()
+
+    # write some data to .csv
+    timestamp = datetime.now().strftime("%Y-%m-%d_%H%M%S")
+    datadir = ROOT_DIR.joinpath("gen_data")
+    filepath = datadir.joinpath(timestamp + ".csv")
+
+    os.makedirs(datadir, exist_ok=True)
+    df.to_csv(filepath)
--- wcst.py
+++ wcst.py
@@ -1,16 +1,18 @@
+import sys
 import random
-import json
+import numpy as np
 import gym
 from gym import spaces
-import pandas as pd
-import numpy as np
 from itertools import permutations
 from wcst_cards import card_generator
+
+#unused imports
+#import json
+#import pandas as pd
 
 N_DISCRETE_ACTIONS = 4 # pick one of the four discrete cards
 
 class WCST(gym.Env):
-
 
     """WCST environment that follows the OpenAI gym interface"""
     metadata = {'render.modes': ['human']}
@@ -22,27 +24,26 @@ class WCST(gym.Env):
         # Actions are discrete:
         self.action_space = spaces.Discrete(N_DISCRETE_ACTIONS)
         self.card_deck = card_generator()
-        self.current_step = 0
         # Observations are discrete
         self.observation_space = card_generator()
-        self.rule = np.random.choice([0, 1, 2])
+        # Initialize state
         self.success_counter = 0 # number of correct responses in a row
+        self.current_step = 0
 
+        self.rule = np.random.choice([0, 1, 2])
 
     def _next_observation(self):
         """a card is shown with values of (colour, form, num of elements)"""
-        card = random.choice(self.card_deck) # do we discard used cards? -- no, we have 24 unique cards but at maximum 250 trials
+        card = random.choice(self.card_deck) # do we discard used cards? -- no, we have 24 unique cards but 250 trials
         return card
 
-    # def _take_action(self):
-    #     """agent picks one of the four cards based on predefined policy"""
-    #     # WIP
+    def _take_action(self, action):
+        """update environment based on action given by agent"""
+        # WIP
 
     def _calculate_reward(self, action):
         # the true rule is not part of the observation?
-
-        right_action = self.rule
-        reward = +1 if action == right_action else -1
+        reward = +1 if action == self.rule else -1
 
         return reward
 
@@ -55,23 +56,24 @@ class WCST(gym.Env):
         else:
             pass
 
-        action = action
+        self._take_action(action)
+
+        obs = self._next_observation()
         reward = self._calculate_reward(action)
 
         self.current_step += 1
-
         if reward == 1:
             self.success_counter += 1 # count the number of correct moves in a row
         else:
             self.success_counter = 0 # reset after wrong move
         done = self.current_step >= 250  # the game is over after 250 steps
 
-        obs = self._next_observation()
         return action, reward, obs, done
 
     def reset(self):
         """reset the state of the environment to the initial state"""
         self.success_counter = 0 # number of correct responses in a row
+        self.current_step = 0
         return self._next_observation()
 
     def render(self, mode="human", close=False):
