bc653a2e3fb8e0a5f63bc8738a6cd78e60ebcec4
sudraka <sudraka.69@gmail.com>
2021-08-14 13:01:43 +0530

    Minor edit

--- wcst.py
+++ wcst.py
@@ -101,7 +101,7 @@ class WCST(gym.Env):
         self.current_step = 0
         self.rule = np.random.choice([0, 1, 2])
         # self.right_action = map_rule_to_action(self.card, self.rule)
-        # NOTE Don't need right_action at initialization.
+        # NOTE Don't need right_action at initialization/reset.
         self.success_counter = 0  # reset success success_counter
         self.switch_counter = 0  # reset rule switch counter
 
