1c0418f8c4aa70779ac06b6f9567c9b419154b00
blob404 <blob404@protonmail.ch>
2021-08-16 16:34:18 +0200

    move replay_buffer to helper_fucntions

--- helper_functions.py
+++ helper_functions.py
@@ -1,5 +1,6 @@
 from itertools import permutations
 import numpy as np
+import collections
 
 def card_generator(li_values=[0, 1, 2, 3], length=3):
     """generate cards given a list of values and the length of tuple."""
@@ -35,3 +36,46 @@ def create_rule_series(total_time):
     rule_series = rule_series[:total_time]
 
     return rule_series
+
+# Create a convenient container for the SARS tuples required by NFQ.
+Transitions = collections.namedtuple(
+    'Transitions', ['state', 'action', 'reward', 'discount', 'next_state'])
+
+# Simple replay buffer
+class ReplayBuffer(object):
+    """A simple Python replay buffer."""
+
+    def __init__(self, capacity: int = None):
+        self.buffer = collections.deque(maxlen=capacity)
+        self._prev_state = None
+
+    # TODO change timestep format to that of gym env
+    # def add_first(self, initial_timestep: dm_env.TimeStep):
+    #     self._prev_state = initial_timestep.observation
+
+    # def add(self, action: int, timestep: dm_env.TimeStep):
+    #     transition = Transitions(
+    #         state=self._prev_state,
+    #         action=action,
+    #         reward=timestep.reward,
+    #         discount=timestep.discount,
+    #         next_state=timestep.observation,
+    #     )
+    #     self.buffer.append(transition)
+    #     self._prev_state = timestep.observation
+
+    def sample(self, batch_size: int) -> Transitions:
+        # Sample a random batch of Transitions as a list.
+        batch_as_list = random.sample(self.buffer, batch_size)
+
+        # Convert the list of `batch_size` Transitions into a single Transitions
+        # object where each field has `batch_size` stacked fields.
+        return tree_utils.stack_sequence_fields(batch_as_list)
+
+    def flush(self) -> Transitions:
+        entire_buffer = tree_utils.stack_sequence_fields(self.buffer)
+        self.buffer.clear()
+        return entire_buffer
+
+    def is_ready(self, batch_size: int) -> bool:
+        return batch_size <= len(self.buffer)
