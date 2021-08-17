7558a77a9ac5d592499fcbc29dfe12003a003f44
sudraka <sudraka.69@gmail.com>
2021-08-17 10:39:03 +0530

    Update sarsa, introduce memory in similar fashion.

--- models/sarsa.py
+++ models/sarsa.py
@@ -1,9 +1,8 @@
-## TODO Create a one-step on-policy SARSA
+## DONE Create a on-policy SARSA with memory.
 
-import typing
 import numpy as np
-from typing import Callable, Sequence
-import pandas as pd
+import helper_functions
+from itertools import product
 
 
 class Agent:
@@ -13,18 +12,36 @@ class Agent:
         policy=None,
         step_size=0.1,
         discount_factor=0.9,
-        epsilon=0.1,
+        epsilon=0.05,
     ):
+        #
+        #       Initial rendering
+        print(
+            f"agent init w/ step_size {step_size}, epsilon {epsilon}, and discount {discount_factor}"
+        )
 
         # Get size of state and action space from the environment
-        self._num_states = env.observation_space.n
+        self._num_obs = env.observation_space.n
         self._num_actions = env.action_space.n
+        self._streak_memory = 6
 
-        # Create a table of Q-values with card tuples as row indexes.
-        self._q = pd.DataFrame(
-            index=env.card_deck,
-            data=np.zeros((self._num_states, self._num_actions)),
+        # Get list of possible states (using Cartesian product)
+        q_index = list(
+            product(
+                env.card_deck, range(self._num_actions), range(self._streak_memory + 1)
+            )
         )
+
+        # Create a map of Q-values with possible states as indexes.
+        self._q = {}
+        for idx in q_index:
+            self._q[idx] = np.zeros((self._num_actions, 1))
+
+        self._num_states = len(q_index)
+
+        #     index=env.card_deck,
+        #     data=np.zeros((self._num_states, self._num_actions)),
+        # )
 
         # Store algorithm hyper-parameters.
         self._step_size = step_size
@@ -38,66 +55,90 @@ class Agent:
         self._behaviour_policy = policy
 
         # Initialize state
-        self._state = env._next_observation()
-        self._action = None
+        self._obs = env.card
+        # Initialize action (which card is picked)
+        self._action = 0
+        # Map action to rule (which category was picked on previous attempt)
+        self._rule = 0
+        # Get number of successive correct answers
+        self._streak = 0
 
     def q_values(self, state):
         return self._q
 
-    def select_action(self, state, policy=None):
+    def get_state(self):
+        state = (self._obs, self._rule, self._streak)
+        return state
 
-        if policy == None:
+    def select_action(self, state):
+
+        if self._behaviour_policy == None:
             # Default policy: random action
             # Exploration
             action = np.random.randint(low=0, high=self._num_actions)
 
-        elif policy == "simple-table-lookup":
+        elif self._behaviour_policy == "value_max":
             # Select action by just looking at Q-table
             # Exploitation
-            action = self._q[state].argmax()  # wrong indexing
+            action = np.argmax(self._q[state])
 
-        elif policy == "epsilon-greedy":
+        elif self._behaviour_policy == "epsilon_greedy":
             # Select action based on the epsilon-greedy policy
             # Finding out the exploration-exploitation balance
             if self._epsilon < np.random.random():
-                action = self._q[state].argmax()  # wrong indexing
+                action = np.argmax(self._q[state])
             else:
                 action = np.random.randint(low=0, high=self._num_actions)
 
         # TODO implement other policies later
         return action
 
-    def _td_error_sarsa(self, s, a, r, g, next_s, next_a):
+    def _td_error(self, s, a, r, g, next_s):
         # Compute the TD error given the learning is on policy.
-        next_q = self._q.loc[[next_s], next_a].values
-        cur_q = self._q.loc[[s], a].values
+
+        next_a = self.select_action(next_s)  # Best future action based on the present
+
+        next_q = self._q[next_s][next_a]
+        cur_q = self._q[s][a]
         tde = r + g * next_q - cur_q
         return tde
 
     def render(self):
-        self._env.render()
-        print(f"New state: {self._state}")
-        print(f"Prev. action: {self._action}")
+        # self._env.render()
+        # print(f"New state: {self._state}")
+        # print(f"Prev. action: {self._action}")
+        pass
 
     def update(self):
-        # Get action based on policy
-        s = self._state
+
+        # Create complete state representation
+        s = self.get_state()
+
+        # Update current action based on policy
         a = self.select_action(s)
 
-        # Update environment, get next_s and reward as observations
-        a, r, next_s, _ = self._env.step(a)
+        # Update environment, get next card and reward
+        r, next_obs, _, _ = self._env.step(a)
 
-        # Calculate the next action that would be taken
-        next_a = self.select_action(next_s)
+        # Update internal streak count
+        if r == 1:
+            self._streak = min(self._streak + 1, self._streak_memory)
+        else:
+            self._streak = 0
+
+        # Update the current state.
+        self._action = a
+        self._rule = helper_functions.map_action_to_rule(self._obs, self._action)
+        self._obs = next_obs
+
+        # Get updated state vector
+        next_s = self.get_state()
+
         # Get discount factor applied on future rewards
         g = self._discount_factor
 
         # Compute Temporal Difference error (TDE)
-        tde = self._td_error(s, a, r, g, next_s, next_a)
+        tde = self._td_error(s, a, r, g, next_s)
 
         # Update the Q-value table value at (s, a).
-        self._q.loc[[s], a] += self._step_size * tde
-
-        # Update the current state.
-        self._state = next_s
-        self._action = a
+        self._q[s][a] += self._step_size * tde
